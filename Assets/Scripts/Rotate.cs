da6514c8a5f99dd98b062ff6a9d62f055647da07
SHMAN12345 <shong-wm20@student.tarc.edu.my>
2023-05-09 18:43:38 +0800

    LoadScene but cacat

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotationSpeed = 10.0f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string sceneName;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+     
+    }
+
+    public void SceneLoad()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
