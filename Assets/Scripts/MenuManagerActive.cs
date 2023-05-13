2bddc234113d159d87580d0a3e5530f82757bfe3
SHMAN12345 <shong-wm20@student.tarc.edu.my>
2023-05-13 17:01:00 +0800

    Room/Audio Commit

--- Assets/Scripts/MenuManagerActive.cs
+++ Assets/Scripts/MenuManagerActive.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ActiveUI : MonoBehaviour
+
+public class MenuManagerActive : MonoBehaviour
 {
     public Transform head;
     public float spawnDistance = 2;
-    public GameObject menu;
+    public GameObject menu,setting,gallery;
     public InputActionProperty showButton;
-    // Start is called before the first frame update
+
     private Camera _cam;
 
     private void Start()
@@ -17,15 +18,25 @@ public class ActiveUI : MonoBehaviour
         _cam = Camera.main;
     }
 
+    // Start is called before the first frame update
+   
     // Update is called once per frame
     void Update()
     {
         if (showButton.action.WasPressedThisFrame())
         {
+
             menu.SetActive(!menu.activeSelf);
             menu.transform.position = head.position + new Vector3(head.forward.x, 1, head.forward.z).normalized * spawnDistance;
             menu.transform.rotation = Quaternion.LookRotation(menu.transform.position - _cam.transform.position);
             //menu.transform.rotation = Quaternion.LookRotation(menu.transform.position - head.position, Vector3.up);
+
+            setting.transform.position = menu.transform.position;
+            setting.transform.rotation = menu.transform.rotation;
+
+            gallery.transform.position=menu.transform.position;
+            gallery.transform.rotation=menu.transform.rotation;
         }
     }
 }
+
